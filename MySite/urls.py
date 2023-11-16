1cac1db1febf4c2e87093634343d1e1bc5634329
Darshil Thakkar <121124627+DarshilThakkar@users.noreply.github.com>
2023-11-17 00:04:58 +0530

    changes

--- MySite/urls.py
+++ MySite/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
