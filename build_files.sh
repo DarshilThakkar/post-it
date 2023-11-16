5a8c2e0827df02f3985ba90b45a21ad67fa32058
Darshil Thakkar <121124627+DarshilThakkar@users.noreply.github.com>
2023-11-17 00:34:12 +0530

    last

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-pip freeze > requirements.txt
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+echo " BUILD START"
+python3.9 -m pip install -r requirements.txt 
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END"
